e46c391db4d1bfaafdc4fc7eeee09aafb2764d9f
Jingchao Lee <satzcoal@hotmail.com>
2016-08-31 16:07:14 +0800

    a

---
+++ app/admin/brand.rb
@@ -0,0 +1,24 @@
+ActiveAdmin.register Brand do
+  permit_params :name, :country_id
+
+  index do
+    selectable_column
+    id_column
+    column :name
+    column :country_id
+    column :created_at
+    actions
+  end
+
+  filter :name
+  filter :country_id
+  filter :created_at
+
+  form do |f|
+    f.inputs "Brand Details" do
+      f.input :name
+      f.input :country_id
+    end
+    f.actions
+  end
+end
