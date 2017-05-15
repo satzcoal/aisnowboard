52c1c480aba7f90e8aef5aaa282f3f9f30a2bb3c
李靖超 <satzcoal@hotmail.com>
2017-05-15 20:57:57 +0800

    Revert "Dev (#1)"
    This reverts commit abc8ec39b985e8f64eebf3ccfcc6b36d4e2fce20.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,17 +1,6 @@
 class Product < ApplicationRecord
-  validates_presence_of :name, :category_id, :brand_id
+  validates_presence_of :name, :category_id, :brand_id, :is_deleted, :ori_id
 
   belongs_to :category
   belongs_to :brand
-
-  has_many :pictures
-  has_one :main_picture, -> { order(:s_index) }, class_name: 'Picture'
-
-  def as_json(options = {})
-    super((options || {})).merge({:url => self.url})
-  end
-
-  def url
-    main_picture.try(:url)
-  end
 end
